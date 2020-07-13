55b0fb61cfe7d20dc33fdafe14eaeb2ef405437f
bocip <ciprianboziean@gmail.com>
2020-07-13 22:03:13 +0100

    Solved helloWorld, repeatString and reverseString exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+    let result = string;
+    if (number < 0) {
+        return "ERROR";
+    }
 
+    let i = 0;
+    while (i <= number) {
+        return result.repeat(number);
+        i++;
+    }
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    var myString = "";
 
+    return string.split('').reverse().join('')
 }
 
 module.exports = reverseString
